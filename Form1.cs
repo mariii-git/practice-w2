f09b53f7a4ac94f955afacfd31081ebb2d9cd2fc
mariiiiiii <masum.s1343@gmail.com>
2025-11-07 08:16:57 -0800

    Add files via upload
    یک پروژه از ویندوز فرم ها مبحث مسیج باکس

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult a = MessageBox.Show("ایا میخواهید خارج شوید ؟", "warning", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            if (a == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
